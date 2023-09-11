fdb952e7bf3037087210063c48bbab2a193553b2
chrisbooth <chris.booth.ct@gmail.com>
2023-09-11 17:18:04 +0200

    Updated Tic Tac Toe game

--- src/rockPaperScissors.java
+++ src/rockPaperScissors.java
@@ -17,55 +17,55 @@ public class rockPaperScissors {
 		// The user is asked to input a number from 0 to 2 - with each number representing rock(1), paper(2) or scissors(0).
 		System.out.println("Enter 0 for scissors, 1 for rock or 2 for paper: " );
 		
-		// The Scanner is used to allow the user to input a number in 'user_choice'.
-		Scanner s = new Scanner(System.in);
-		
-		int user_choice = s.nextInt();
-		
-		
-		/* 
-		 	A series of else if statements run to check both the program's randomly generated number and the number chosen by the user.
-			It then prints out who won based on whether rock (1) beats scissors (0), scissors beats paper (2), or paper beats rock.
-			If what the user chooses is the same as the randomly generated number, then a tie is declared.
-		*/
-		if (RockPaperScissors == 1 && user_choice == 0) {
+		try (// The Scanner is used to allow the user to input a number in 'user_choice'.
+		Scanner s = new Scanner(System.in)) {
+			int user_choice = s.nextInt();
 			
-			System.out.println("You chose scissors, computer chose rock: ");
-			System.out.println("Rock beats scissors - you lose!");		
-		
-		} else if (RockPaperScissors == 1 && user_choice == 2) {
 			
-			System.out.println("You chose paper, computer chose rock: ");
-			System.out.println("Paper beats rock - you win!");
-		
-		} else if (RockPaperScissors == 2 && user_choice == 1) {
-		
-			System.out.println("You chose rock, computer chose paper: ");
-			System.out.print("Paper beats rock - you lose!");
-		
-		} else if (RockPaperScissors == 2 && user_choice == 0) {
-		
-			System.out.println("You chose scissors, computer chose paper: ");
-			System.out.println("Scissors beats paper - you win!");
-		
-		} else if (RockPaperScissors == 0 && user_choice == 1) {
-		
-			System.out.println("You chose rock, computer chose scissors: ");
-			System.out.println("Rock beats scissors - you win!");
-		
-		} else if (RockPaperScissors == 0 && user_choice == 2) {
-		
-			System.out.println("You chose paper, computer chose scissors: ");
-			System.out.println("Scissors beats paper - you lose!");
-		
-		} else if (RockPaperScissors == user_choice) {
-		
-			System.out.println("You and the computer both chose the same: ");
-			System.out.println("Let's call this a draw");
-		
-		// If the user has entered anything other than 0, 1, or 2, then this statement runs.
-		} else {
-			System.out.println("You have entered an invalid option.");
+			/* 
+			 	A series of else if statements run to check both the program's randomly generated number and the number chosen by the user.
+				It then prints out who won based on whether rock (1) beats scissors (0), scissors beats paper (2), or paper beats rock.
+				If what the user chooses is the same as the randomly generated number, then a tie is declared.
+			*/
+			if (RockPaperScissors == 1 && user_choice == 0) {
+				
+				System.out.println("You chose scissors, computer chose rock: ");
+				System.out.println("Rock beats scissors - you lose!");		
+			
+			} else if (RockPaperScissors == 1 && user_choice == 2) {
+				
+				System.out.println("You chose paper, computer chose rock: ");
+				System.out.println("Paper beats rock - you win!");
+			
+			} else if (RockPaperScissors == 2 && user_choice == 1) {
+			
+				System.out.println("You chose rock, computer chose paper: ");
+				System.out.print("Paper beats rock - you lose!");
+			
+			} else if (RockPaperScissors == 2 && user_choice == 0) {
+			
+				System.out.println("You chose scissors, computer chose paper: ");
+				System.out.println("Scissors beats paper - you win!");
+			
+			} else if (RockPaperScissors == 0 && user_choice == 1) {
+			
+				System.out.println("You chose rock, computer chose scissors: ");
+				System.out.println("Rock beats scissors - you win!");
+			
+			} else if (RockPaperScissors == 0 && user_choice == 2) {
+			
+				System.out.println("You chose paper, computer chose scissors: ");
+				System.out.println("Scissors beats paper - you lose!");
+			
+			} else if (RockPaperScissors == user_choice) {
+			
+				System.out.println("You and the computer both chose the same: ");
+				System.out.println("Let's call this a draw");
+			
+			// If the user has entered anything other than 0, 1, or 2, then this statement runs.
+			} else {
+				System.out.println("You have entered an invalid option.");
+			}
 		}
 		
 	}
--- src/ticTacToe.java
+++ src/ticTacToe.java
@@ -1,9 +1,60 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class ticTacToe {
 
+    static String[] board;
+    static String turnPlayer;
+    static int turn;
 
-    public void printBoard(String[] currentBoard){
+    public static String checkWinner(String[] board, int turn) {
+
+        for (int i = 0; i < 8; i++) {
+
+            String line = null;
+
+            switch (i) {
+                case 0:
+                    line = board[0] + board[1] + board[2];
+                    break;
+                case 1:
+                    line = board[3] + board[4] + board[5];
+                    break;
+                case 2:
+                    line = board[6] + board[7] + board[8];
+                    break;
+                case 3:
+                    line = board[0] + board[3] + board[6];
+                    break;
+                case 4:
+                    line = board[1] + board[4] + board[7];
+                    break;
+                case 5:
+                    line = board[2] + board[5] + board[8];
+                    break;
+                case 6:
+                    line = board[0] + board[4] + board[8];
+                    break;
+                case 7:
+                    line = board[2] + board[4] + board[6];
+                    break;
+            }
+
+            if (line.equals("XXX")) {
+                return "X";
+            } else if (line.equals("OOO")) {
+                return "O";
+            }
+
+        }
+        if (turn == 9) {
+            return "draw";
+        }
+
+        return null;
+
+    }
+
+    public static void printBoard(String[] currentBoard) {
 
         System.out.println("--------------");
         System.out.println("| " + currentBoard[0] + " | " + currentBoard[1] + " | " + currentBoard[2] + " |");
@@ -13,32 +64,68 @@ public class ticTacToe {
         System.out.println("| " + currentBoard[6] + " | " + currentBoard[7] + " | " + currentBoard[8] + " |");
         System.out.println("--------------");
 
+    }
+
+    public static String currentPlayer(String turn) {
+
+        if (turn == "X") {
+            return "O";
+        } else {
+            return "X";
+        }
 
     }
 
-    public int[] currentBoard(int[] player){
-
-        
-        
-        return player;
-
-
-
-    }
-
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
         Scanner input = new Scanner(System.in);
-        String[] board = new String[9];
-        String turn = "O";
+        board = new String[9];
+        turnPlayer = "O";
+        turn = 0;
         String winner = null;
 
-        
+        for (int i = 0; i < 9; i++) {
+            board[i] = String.valueOf(i + 1);
+        }
 
+        System.out.println("Let's play some Tic Tac Toe!");
+        printBoard(board);
+        System.out.println("Os goes first. Input your choice:");
 
+        while (winner == null) {
+            turn += 1;
+            int inputNum;
+
+            try {
+                inputNum = input.nextInt();
+                if (inputNum < 0 && inputNum > 9) {
+                    System.out.println("Invalid input. Please try again");
+                    continue;
+                }
+            } catch (InputMismatchException e) {
+                System.out.println("Invalid input. Please try again");
+                continue;
+            }
+
+            if (board[inputNum - 1].equals(String.valueOf(inputNum))) {
+                board[inputNum - 1] = turnPlayer;
+                turnPlayer = currentPlayer(turnPlayer);
+
+                printBoard(board);
+                winner = checkWinner(board, turn);
+            } else {
+                System.out.println("Slot already taken. Try again:");
+            }
+
+        }
+        if (winner.equalsIgnoreCase("draw")) {
+            System.out.println("Looks like it was a draw! Better luck next time!");
+        } else {
+            System.out.println(winner + " is the winner!");
+        }
+
+        input.close();
 
     }
 
-
-    
 }
